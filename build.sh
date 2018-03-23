7e99c72fd4d19820c48ac8b2d539c6e386b9631c
Hugo Rivera <6373447+roguh@users.noreply.github.com>
2018-03-23 16:55:43 -0600

    Add better CSS for HTML version

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 OUTPUT_DIRECTORY=out
 
-HTML='--standalone --self-contained --css style.css'
+HTML='--standalone --self-contained --css killercup.css --css style.css'
 DOCX='--reference-doc=reference.docx'
 TEX='--latex-engine=xelatex'
 
@@ -10,4 +10,4 @@ if [ $# != 2 ] ; then
 fi
 
 mkdir -p $OUTPUT_DIRECTORY
-pandoc $HTML $DOCX $TEX -f markdown+multiline_tables $1.md -o $OUTPUT_DIRECTORY/$1.$2
+pandoc $HTML $DOCX $TEX --smart -f markdown+multiline_tables $1.md -o $OUTPUT_DIRECTORY/$1.$2
